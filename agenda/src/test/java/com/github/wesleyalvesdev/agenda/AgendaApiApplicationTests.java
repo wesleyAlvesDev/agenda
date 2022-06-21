c2a117db96cfb0a9770346f7e966d69f954e408b
Wesley Alves <wesley.alves@lightbase.com.br>
2022-06-21 18:37:47 -0300

    agenda api

---
+++ agenda/src/test/java/com/github/wesleyalvesdev/agenda/AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyalvesdev.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
